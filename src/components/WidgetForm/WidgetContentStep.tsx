96d15645c83f5d935566453ecadce89213589154
Morgana <morganabagatini@gmail.com>
2022-05-03 22:28:08 -0300

    ajuste classes

--- src/components/WidgetForm/WidgetContentStep.tsx
+++ src/components/WidgetForm/WidgetContentStep.tsx
@@ -39,14 +39,14 @@ export function WidgetContentStep({
 
       <form className="my-4 w-full">
         <textarea
-          className="min-w-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          className="min-w-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
         />
 
         <footer className="flex gap-2 mt-2">
           <button
             type="button"
-            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            className="p-2 bg-zinc-800 rounded-md border-transparent transition-colors hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
           >
             <Camera className="w-6 h-6" />
           </button>
